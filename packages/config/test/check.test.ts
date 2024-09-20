15d49af516a18e4627f61322d40788d17b784384
tmastrom <thomas.mastromonaco@gmail.com>
2024-09-20 10:07:20 +0300

    feat: define children type in typebox

--- packages/config/test/check.test.ts
+++ packages/config/test/check.test.ts
@@ -3,11 +3,11 @@ import { describe, expect, it } from 'vitest'
 import { check } from '../src/index'
 
 describe('check', () => {
-  it.only('checks the valid json file', () => {
+  it('checks the valid json file', () => {
     const result = check('/test/valid.json')
     console.log(JSON.stringify(result, null, 3))
     expect(result.valid).toBe(true)
-    expect(result.errors).toBe(null)
+    expect(result.errors.length).toBe(0)
   })
   it('checks the invalid json file', () => {
     const result = check('/test/invalid.json')
