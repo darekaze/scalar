21f43a9d0cf2af6f6628045299dd751235a23ef7
tmastrom <thomas.mastromonaco@gmail.com>
2024-09-21 18:38:19 +0300

    test: config check function

--- packages/config/test/check.test.ts
+++ packages/config/test/check.test.ts
@@ -1,25 +1,29 @@
+import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
 
 import { check } from '../src/index'
 
 describe('check', () => {
   it('checks the valid json file', () => {
-    const result = check('/test/valid.json')
-    console.log(JSON.stringify(result, null, 3))
+    const file = fileURLToPath(new URL('valid.json', import.meta.url))
+    const result = check(file)
+
     expect(result.valid).toBe(true)
     expect(result.errors.length).toBe(0)
   })
   it('checks the invalid json file', () => {
-    const result = check('/test/invalid.json')
-    console.log(JSON.stringify(result, null, 3))
-    expect(result.valid).toBe(false)
+    const file = fileURLToPath(new URL('./invalid.json', import.meta.url))
+    const result = check(file)
 
-    console.log(result.errors)
-    expect(result.errors).toBeDefined()
+    expect(result.valid).toBe(false)
     expect(result.errors.length).toEqual(6)
   })
   it('checks the invalid json file missing subdomain', () => {
-    const result = check('/test/invalid-subdomain.json')
+    const file = fileURLToPath(
+      new URL('./invalid-subdomain.json', import.meta.url),
+    )
+    const result = check(file)
+
     expect(result.valid).toBe(false)
     expect(result.errors.length).toEqual(2)
     expect(result.errors[0].message).toEqual('Expected required property')
